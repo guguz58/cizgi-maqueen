742c727f0e02c5be9f719c68eb317e4ed02cab81
GÜNAY GÜZEL <102692490+guguz58@users.noreply.github.com>
2025-01-17 14:07:50 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+basic.showIcon(IconNames.No)
+basic.pause(1000)
+basic.clearScreen()
+led.enable(false)
+basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 200)
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 100)
+    } else {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 12)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 12)
+    }
+})
